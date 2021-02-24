13d6837424eaa065b17bfecf046a0bbdf86ea8b4
rupert <rupert.voelcker@bt.com>
2021-02-24 20:57:12 +0000

    TodoParse: parse multiple todos

--- src/todo.test.ts
+++ src/todo.test.ts
@@ -4,19 +4,32 @@ test('parses a simple uncompleted markdown TODO', async () => {
   const markdown = "- [ ] I need doing";
   const todo = obsidianTodo.parse(markdown);
 
-  expect(todo).toEqual({title: "I need doing", isComplete: false})
+  expect(todo).toEqual([{title: "I need doing", isComplete: false}]);
 });
 
 test('parses a simple completed markdown TODO', async () => {
   const markdown = "- [x] I have been done";
   const todo = obsidianTodo.parse(markdown);
 
-  expect(todo).toEqual({title: "I have been done", isComplete: true})
+  expect(todo).toEqual([{title: "I have been done", isComplete: true}]);
 });
 
 test('parses an indented markdown TODO', async () => {
   const markdown = "    - [ ] I need doing";
   const todo = obsidianTodo.parse(markdown);
 
-  expect(todo).toEqual({title: "I need doing", isComplete: false})
+  expect(todo).toEqual([{title: "I need doing", isComplete: false}]);
+});
+
+test('parses multiple uncompleted markdown TODOs', async () => {
+  const markdown = `- [ ] thing one
+  - [x] thing two
+  - [ ] thing three`;
+  const todo = obsidianTodo.parse(markdown);
+
+  expect(todo).toEqual([
+    {title: "thing one", isComplete: false},
+    {title: "thing two", isComplete: true},
+    {title: "thing three", isComplete: false},
+  ]);
 });
--- src/todo.ts
+++ src/todo.ts
@@ -1,7 +1,7 @@
 // https://codemix.com/opaque-types-in-javascript/
 type Opaque<K, T> = T & { __TYPE__: K };
 
-const matcher = /- \[(.)\]\s(.*)/
+const matcher = /- \[(.)\]\s(.*)/g
 
 export type Todo = Opaque<'Todo', { }>;
 
@@ -10,7 +10,17 @@ type $Todo = Todo & {
   isComplete: boolean
 };
 
-export function parse(markdown: string): Todo {
-  const [ , mark, title] = markdown.match(matcher);
-  return ({title: title, isComplete: mark === 'x'} as $Todo) as Todo;
+function buildTodo(matchArray: RegExpMatchArray): Todo {
+  const todoFields = {
+    title: matchArray[2],
+    isComplete: matchArray[1] === 'x'
+  };
+
+  return (todoFields as $Todo) as Todo;
+}
+
+export function parse(markdown: string): Todo[] {
+  const matchArrays = [...markdown.matchAll(matcher)];
+
+  return matchArrays.map(buildTodo);
 }
