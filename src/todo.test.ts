01bb2fc7f689cb79bea7d613407d20ba660b9635
rupert <rupert.voelcker@bt.com>
2021-02-24 19:52:46 +0000

    TodoParse: parse basic Todo from string

--- src/todo.test.ts
+++ src/todo.test.ts
@@ -1,5 +1,22 @@
-import { todo } from './todo';
+import * as obsidianTodo from './todo';
 
-test('I can run a test', async () => {
-  expect(true).toEqual(true)
+test('parses a simple uncompleted markdown TODO', async () => {
+  const markdown = "- [ ] I need doing";
+  const todo = obsidianTodo.parse(markdown);
+
+  expect(todo).toEqual({title: "I need doing", isComplete: false})
+});
+
+test('parses a simple completed markdown TODO', async () => {
+  const markdown = "- [x] I have been done";
+  const todo = obsidianTodo.parse(markdown);
+
+  expect(todo).toEqual({title: "I have been done", isComplete: true})
+});
+
+test('parses an indented markdown TODO', async () => {
+  const markdown = "    - [ ] I need doing";
+  const todo = obsidianTodo.parse(markdown);
+
+  expect(todo).toEqual({title: "I need doing", isComplete: false})
 });
--- src/todo.ts
+++ src/todo.ts
@@ -1,15 +1,16 @@
 // https://codemix.com/opaque-types-in-javascript/
-
 type Opaque<K, T> = T & { __TYPE__: K };
+
+const matcher = /- \[(.)\]\s(.*)/
 
 export type Todo = Opaque<'Todo', { }>;
 
-export function todo(input: { title: string, isComplete: boolean }) {
-  return (input as $Todo) as Todo;
-}
-
-// internal - not accessible outside this file
 type $Todo = Todo & {
   title: string,
   isComplete: boolean
 };
+
+export function parse(markdown: string): Todo {
+  const [ , mark, title] = markdown.match(matcher);
+  return ({title: title, isComplete: mark === 'x'} as $Todo) as Todo;
+}
