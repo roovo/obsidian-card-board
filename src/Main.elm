3b72960b8bbefd8cc871aa052468cba00a770609
rupert <rupert.voelcker@bt.com>
2021-11-03 09:56:55 +0000

    Refactor(elm): move Board update and view into own module

--- src/Main.elm
+++ src/Main.elm
@@ -6,14 +6,13 @@ import Card exposing (Card)
 import Date exposing (Date)
 import FeatherIcons
 import Html exposing (Html)
-import Html.Attributes exposing (checked, class, hidden, id, type_)
-import Html.Events exposing (onClick)
-import Html.Keyed
+import Html.Attributes exposing (class)
 import InteropDefinitions
 import InteropPorts
 import Json.Decode as JD
 import MarkdownFile exposing (MarkdownFile)
 import Model exposing (Model)
+import Page.Board as BoardPage
 import Page.Settings as SettingsPage
 import Panel exposing (Panel)
 import Panels exposing (Panels)
@@ -56,14 +55,10 @@ init flags =
 type Msg
     = BadInputFromTypeScript
     | BoardConfigsUpdated (List BoardConfig)
+    | GotBoardPageMsg BoardPage.Msg
     | GotSettingsPageMsg SettingsPage.Msg
     | InitCompleted
     | ReceiveTime ( Time.Zone, Time.Posix )
-    | SettingsClicked
-    | TabSelected Int
-    | TaskItemEditClicked String
-    | TaskItemDeleteClicked String
-    | TaskItemToggled String
     | Tick Time.Posix
     | VaultFileAdded MarkdownFile
     | VaultFileDeleted String
@@ -89,6 +84,10 @@ update msg model =
                 ]
             )
 
+        ( GotBoardPageMsg subMsg, _ ) ->
+            BoardPage.update subMsg model
+                |> updateWith GotBoardPageMsg
+
         ( GotSettingsPageMsg subMsg, _ ) ->
             SettingsPage.update subMsg model
                 |> updateWith GotSettingsPageMsg
@@ -103,36 +102,6 @@ update msg model =
 
         ( ReceiveTime ( zone, posix ), _ ) ->
             ( { model | timeWithZone = { zone = zone, now = posix } }, Cmd.none )
-
-        ( SettingsClicked, _ ) ->
-            ( { model | configBeingEdited = Model.Editing model.boardConfigs }, Cmd.none )
-
-        ( TabSelected tabIndex, _ ) ->
-            ( { model | boardConfigs = SafeZipper.atIndex tabIndex model.boardConfigs }, Cmd.none )
-
-        ( TaskItemDeleteClicked id, _ ) ->
-            ( model, cmdIfHasTask id model InteropPorts.deleteTask )
-
-        ( TaskItemEditClicked id, _ ) ->
-            ( model, cmdIfHasTask id model InteropPorts.openTaskSourceFile )
-
-        ( TaskItemToggled id, _ ) ->
-            case model.taskList of
-                State.Loaded taskList ->
-                    case TaskList.taskContainingId id taskList of
-                        Just matchingItem ->
-                            ( model
-                            , InteropPorts.rewriteTasks
-                                model.timeWithZone
-                                (TaskItem.filePath matchingItem)
-                                (TaskItem.tasksToToggle id model.timeWithZone matchingItem)
-                            )
-
-                        Nothing ->
-                            ( model, Cmd.none )
-
-                _ ->
-                    ( model, Cmd.none )
 
         ( Tick time, _ ) ->
             ( { model | timeWithZone = TimeWithZone.now time model.timeWithZone }
@@ -186,24 +155,6 @@ cmdForTaskRedraws newTasks model =
 
     else
         Cmd.none
-
-
-
--- cmdIfHasTask : String -> Model -> ({ a | filePath : String, lineNumber : Int, originalText : String } -> Cmd msg) -> Cmd Msg
-
-
-cmdIfHasTask id model cmd =
-    case model.taskList of
-        State.Loaded taskList ->
-            case TaskList.taskFromId id taskList of
-                Just matchingItem ->
-                    cmd <| TaskItem.fields matchingItem
-
-                Nothing ->
-                    Cmd.none
-
-        _ ->
-            Cmd.none
 
 
 rePathedTaskItems : String -> String -> State TaskList -> TaskList
@@ -281,309 +232,14 @@ view : Model -> Html Msg
 view model =
     case model.taskList of
         State.Loaded taskList ->
-            let
-                panels =
-                    Panels.init model.boardConfigs taskList
-
-                currentIndex =
-                    Panels.currentIndex panels
-            in
             Html.div [ class "card-board" ]
                 [ Html.div [ class "card-board-container" ]
-                    [ Html.ul [ class "card-board-tab-list" ]
-                        (tabHeaders currentIndex panels)
-                    , Html.div [ class "card-board-panels" ]
-                        (Panels.panels panels
-                            |> SafeZipper.indexedMapSelectedAndRest (selectedPanelView model.timeWithZone) (panelView model.timeWithZone)
-                            |> SafeZipper.toList
-                        )
+                    [ BoardPage.view model.timeWithZone model.boardConfigs taskList
+                        |> Html.map GotBoardPageMsg
+                    , SettingsPage.dialogs model.configBeingEdited
+                        |> Html.map GotSettingsPageMsg
                     ]
-                , SettingsPage.dialogs model.configBeingEdited
-                    |> Html.map GotSettingsPageMsg
                 ]
 
         _ ->
             Html.text "Loading tasks...."
-
-
-tabHeaders : Maybe Int -> Panels -> List (Html Msg)
-tabHeaders currentIndex panels =
-    let
-        beforeHeaderClass =
-            tabHeaderClass currentIndex -1
-
-        beforeFirst =
-            Html.li [ class <| "card-board-pre-tabs" ++ beforeHeaderClass ]
-                [ Html.div
-                    [ class "card-board-tabs-inner"
-                    , onClick SettingsClicked
-                    ]
-                    [ FeatherIcons.settings
-                        |> FeatherIcons.withSize 1
-                        |> FeatherIcons.withSizeUnit "em"
-                        |> FeatherIcons.toHtml []
-                    ]
-                ]
-
-        afterHeaderClass =
-            tabHeaderClass currentIndex (Panels.length panels)
-
-        afterLast =
-            Html.li [ class <| "card-board-post-tabs" ++ afterHeaderClass ]
-                [ Html.div [ class "card-board-tabs-inner" ]
-                    [ Html.text "" ]
-                ]
-
-        tabs =
-            Panels.tabTitles panels
-                |> SafeZipper.indexedMapSelectedAndRest selectedTabHeader (tabHeader currentIndex)
-                |> SafeZipper.toList
-    in
-    beforeFirst :: List.append tabs [ afterLast ]
-
-
-selectedTabHeader : Int -> String -> Html Msg
-selectedTabHeader _ title =
-    Html.li [ class "card-board-tab-title is-active" ]
-        [ Html.div [ class "card-board-tabs-inner" ]
-            [ Html.text <| title ]
-        ]
-
-
-tabHeader : Maybe Int -> Int -> String -> Html Msg
-tabHeader currentIndex index title =
-    let
-        headerClass =
-            tabHeaderClass currentIndex index
-    in
-    Html.li
-        [ class ("card-board-tab-title" ++ headerClass)
-        , onClick <| TabSelected index
-        ]
-        [ Html.div [ class "card-board-tabs-inner" ]
-            [ Html.text <| title ]
-        ]
-
-
-tabHeaderClass : Maybe Int -> Int -> String
-tabHeaderClass currentIndex index =
-    case currentIndex of
-        Just i ->
-            if index == i - 1 then
-                " is-before-active"
-
-            else if index == i + 1 then
-                " is-after-active"
-
-            else
-                ""
-
-        Nothing ->
-            ""
-
-
-panelView : TimeWithZone -> Int -> Panel -> Html Msg
-panelView timeWithZone index panel =
-    Html.div
-        [ class "card-board-panel"
-        , hidden True
-        ]
-        [ Html.div [ class "card-board-columns" ]
-            (panel
-                |> Panel.columns timeWithZone index
-                |> List.map (\( n, cs ) -> column timeWithZone n cs)
-            )
-        ]
-
-
-selectedPanelView : TimeWithZone -> Int -> Panel -> Html Msg
-selectedPanelView timeWithZone index panel =
-    Html.div [ class "card-board-panel" ]
-        [ Html.div [ class "card-board-columns" ]
-            (panel
-                |> Panel.columns timeWithZone index
-                |> List.map (\( n, cs ) -> column timeWithZone n cs)
-            )
-        ]
-
-
-column : TimeWithZone -> String -> List Card -> Html Msg
-column timeWithZone title cards =
-    Html.div [ class "card-board-column" ]
-        [ Html.div [ class "card-board-column-header" ]
-            [ Html.text title ]
-        , Html.Keyed.ul [ class "card-board-column-list" ]
-            (List.map (cardView timeWithZone) cards)
-        ]
-
-
-cardView : TimeWithZone -> Card -> ( String, Html Msg )
-cardView timeWithZone card =
-    let
-        cardId =
-            Card.id card
-
-        taskItem =
-            Card.taskItem card
-
-        taskItemId =
-            Card.taskItemId card
-
-        highlightAreaClass =
-            case Card.highlight timeWithZone card of
-                Card.HighlightCritical ->
-                    "critical"
-
-                Card.HighlightGood ->
-                    "good"
-
-                Card.HighlightImportant ->
-                    "important"
-
-                Card.HighlightNone ->
-                    ""
-    in
-    Html.li [ class "card-board-card cm-s-obsidian markdown-preview-view" ]
-        [ Html.div [ class ("card-board-card-highlight-area " ++ highlightAreaClass) ]
-            []
-        , Html.div [ class "card-board-card-content-area" ]
-            [ Html.input
-                [ type_ "checkbox"
-                , class "task-list-item-checkbox"
-                , onClick <| TaskItemToggled taskItemId
-                , checked <| TaskItem.isCompleted taskItem
-                ]
-                []
-            , Html.div [ class "card-board-card-title", id cardId ]
-                []
-            , cardTagsView (TaskItem.tags taskItem)
-                |> when (TaskItem.hasTags taskItem)
-            , subtasksView (Card.subtasks card)
-                |> when (TaskItem.hasSubtasks taskItem)
-            , notesView (Card.notesId card)
-                |> when (TaskItem.hasNotes taskItem)
-            , Html.div [ class "card-board-card-footer-area" ]
-                [ taskDueDate (TaskItem.due taskItem)
-                    |> when (TaskItem.isDated taskItem)
-                , cardActionButtons taskItemId (Card.editButtonId card)
-                ]
-            ]
-        ]
-        |> Tuple.pair cardId
-
-
-cardTagsView : List String -> Html Msg
-cardTagsView tags =
-    Html.div [ class "card-board-card-tag-area" ]
-        (List.map cardTagView tags)
-
-
-cardTagView : String -> Html Msg
-cardTagView tagText =
-    Html.div [ class ("card-board-card-tag tag-" ++ String.replace "/" "-" tagText) ]
-        [ Html.span [ class "cm-hashtag-begin cm-hashtag" ]
-            [ Html.text "#" ]
-        , Html.span [ class "cm-list-1 cm-hashtag cm-hashtag-end" ]
-            [ Html.text tagText ]
-        , Html.span [ class "cm-list-1" ]
-            [ Html.text " " ]
-        ]
-
-
-notesView : String -> Html Msg
-notesView notesId =
-    Html.div [ class "card-board-card-notes-area", id notesId ]
-        []
-
-
-subtasksView : List ( String, TaskItem ) -> Html Msg
-subtasksView subtasks =
-    Html.div [ class "card-board-card-subtasks-area" ]
-        [ Html.ul [ class "contains-task-list" ]
-            (List.map subtaskView subtasks)
-        ]
-
-
-subtaskView : ( String, TaskItem ) -> Html Msg
-subtaskView ( uniqueId, subtask ) =
-    Html.li [ class "card-board-card-subtask task-list-item" ]
-        [ Html.input
-            [ type_ "checkbox"
-            , class "task-list-item-checkbox"
-            , onClick <| TaskItemToggled <| TaskItem.id subtask
-            , checked <| TaskItem.isCompleted subtask
-            ]
-            []
-        , Html.div [ class "card-board-card-title", id uniqueId ]
-            []
-        ]
-
-
-taskDueDate : Maybe Date -> Html Msg
-taskDueDate dueDate =
-    Html.div [ class "card-board-card-action-area-due" ]
-        [ Html.text ("Due: " ++ dueDateString dueDate)
-        ]
-
-
-dueDateString : Maybe Date -> String
-dueDateString dueDate =
-    case dueDate of
-        Just date ->
-            Date.format "E, MMM ddd" date
-
-        Nothing ->
-            "n/a"
-
-
-cardActionButtons : String -> String -> Html Msg
-cardActionButtons taskItemId editButtonId =
-    Html.div [ class "card-board-card-action-area-buttons" ]
-        [ Html.div
-            [ class "card-board-card-action-area-button"
-            , onClickWithPreventDefault <| TaskItemEditClicked taskItemId
-            , id editButtonId
-            ]
-            [ FeatherIcons.edit
-                |> FeatherIcons.withSize 1
-                |> FeatherIcons.withSizeUnit "em"
-                |> FeatherIcons.toHtml []
-            ]
-        , Html.div
-            [ class "card-board-card-action-area-button"
-            , onClick <| TaskItemDeleteClicked taskItemId
-            ]
-            [ FeatherIcons.trash
-                |> FeatherIcons.withSize 1
-                |> FeatherIcons.withSizeUnit "em"
-                |> FeatherIcons.toHtml []
-            ]
-        ]
-
-
-
--- HELPERS
-
-
-onClickWithPreventDefault : msg -> Html.Attribute msg
-onClickWithPreventDefault msg =
-    Html.Events.preventDefaultOn "click" (JD.map alwaysPreventDefault (JD.succeed msg))
-
-
-alwaysPreventDefault : msg -> ( msg, Bool )
-alwaysPreventDefault msg =
-    ( msg, True )
-
-
-empty : Html Msg
-empty =
-    Html.text ""
-
-
-when : Bool -> Html Msg -> Html Msg
-when shouldRender html =
-    if shouldRender then
-        html
-
-    else
-        empty
---
+++ src/Page/Board.elm
@@ -0,0 +1,395 @@
+module Page.Board exposing
+    ( Msg(..)
+    , update
+    , view
+    )
+
+import BoardConfig exposing (BoardConfig)
+import Card exposing (Card)
+import Date exposing (Date)
+import FeatherIcons
+import Html exposing (Html)
+import Html.Attributes exposing (checked, class, hidden, id, type_)
+import Html.Events exposing (onClick)
+import Html.Keyed
+import InteropPorts
+import Json.Decode as JD
+import Model exposing (Model)
+import Panel exposing (Panel)
+import Panels exposing (Panels)
+import SafeZipper exposing (SafeZipper)
+import State
+import TaskItem exposing (TaskItem)
+import TaskList exposing (TaskList)
+import TimeWithZone exposing (TimeWithZone)
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SettingsClicked
+    | TabSelected Int
+    | TaskItemEditClicked String
+    | TaskItemDeleteClicked String
+    | TaskItemToggled String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SettingsClicked ->
+            ( { model | configBeingEdited = Model.Editing model.boardConfigs }, Cmd.none )
+
+        TabSelected tabIndex ->
+            ( { model | boardConfigs = SafeZipper.atIndex tabIndex model.boardConfigs }, Cmd.none )
+
+        TaskItemDeleteClicked id ->
+            ( model, cmdIfHasTask id model InteropPorts.deleteTask )
+
+        TaskItemEditClicked id ->
+            ( model, cmdIfHasTask id model InteropPorts.openTaskSourceFile )
+
+        TaskItemToggled id ->
+            case model.taskList of
+                State.Loaded taskList ->
+                    case TaskList.taskContainingId id taskList of
+                        Just matchingItem ->
+                            ( model
+                            , InteropPorts.rewriteTasks
+                                model.timeWithZone
+                                (TaskItem.filePath matchingItem)
+                                (TaskItem.tasksToToggle id model.timeWithZone matchingItem)
+                            )
+
+                        Nothing ->
+                            ( model, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+
+
+-- cmdIfHasTask : String -> Model -> ({ a | filePath : String, lineNumber : Int, originalText : String } -> Cmd msg) -> Cmd Msg
+
+
+cmdIfHasTask id model cmd =
+    case model.taskList of
+        State.Loaded taskList ->
+            case TaskList.taskFromId id taskList of
+                Just matchingItem ->
+                    cmd <| TaskItem.fields matchingItem
+
+                Nothing ->
+                    Cmd.none
+
+        _ ->
+            Cmd.none
+
+
+
+-- VIEW
+
+
+view : TimeWithZone -> SafeZipper BoardConfig -> TaskList -> Html Msg
+view timeWithZone boardConfigs taskList =
+    let
+        panels =
+            Panels.init boardConfigs taskList
+
+        currentIndex =
+            Panels.currentIndex panels
+    in
+    Html.div []
+        [ Html.ul [ class "card-board-tab-list" ]
+            (tabHeaders currentIndex panels)
+        , Html.div [ class "card-board-panels" ]
+            (Panels.panels panels
+                |> SafeZipper.indexedMapSelectedAndRest (selectedPanelView timeWithZone) (panelView timeWithZone)
+                |> SafeZipper.toList
+            )
+        ]
+
+
+tabHeaders : Maybe Int -> Panels -> List (Html Msg)
+tabHeaders currentIndex panels =
+    let
+        beforeHeaderClass =
+            tabHeaderClass currentIndex -1
+
+        beforeFirst =
+            Html.li [ class <| "card-board-pre-tabs" ++ beforeHeaderClass ]
+                [ Html.div
+                    [ class "card-board-tabs-inner"
+                    , onClick SettingsClicked
+                    ]
+                    [ FeatherIcons.settings
+                        |> FeatherIcons.withSize 1
+                        |> FeatherIcons.withSizeUnit "em"
+                        |> FeatherIcons.toHtml []
+                    ]
+                ]
+
+        afterHeaderClass =
+            tabHeaderClass currentIndex (Panels.length panels)
+
+        afterLast =
+            Html.li [ class <| "card-board-post-tabs" ++ afterHeaderClass ]
+                [ Html.div [ class "card-board-tabs-inner" ]
+                    [ Html.text "" ]
+                ]
+
+        tabs =
+            Panels.tabTitles panels
+                |> SafeZipper.indexedMapSelectedAndRest selectedTabHeader (tabHeader currentIndex)
+                |> SafeZipper.toList
+    in
+    beforeFirst :: List.append tabs [ afterLast ]
+
+
+selectedTabHeader : Int -> String -> Html Msg
+selectedTabHeader _ title =
+    Html.li [ class "card-board-tab-title is-active" ]
+        [ Html.div [ class "card-board-tabs-inner" ]
+            [ Html.text <| title ]
+        ]
+
+
+tabHeader : Maybe Int -> Int -> String -> Html Msg
+tabHeader currentIndex index title =
+    let
+        headerClass =
+            tabHeaderClass currentIndex index
+    in
+    Html.li
+        [ class ("card-board-tab-title" ++ headerClass)
+        , onClick <| TabSelected index
+        ]
+        [ Html.div [ class "card-board-tabs-inner" ]
+            [ Html.text <| title ]
+        ]
+
+
+tabHeaderClass : Maybe Int -> Int -> String
+tabHeaderClass currentIndex index =
+    case currentIndex of
+        Just i ->
+            if index == i - 1 then
+                " is-before-active"
+
+            else if index == i + 1 then
+                " is-after-active"
+
+            else
+                ""
+
+        Nothing ->
+            ""
+
+
+panelView : TimeWithZone -> Int -> Panel -> Html Msg
+panelView timeWithZone index panel =
+    Html.div
+        [ class "card-board-panel"
+        , hidden True
+        ]
+        [ Html.div [ class "card-board-columns" ]
+            (panel
+                |> Panel.columns timeWithZone index
+                |> List.map (\( n, cs ) -> column timeWithZone n cs)
+            )
+        ]
+
+
+selectedPanelView : TimeWithZone -> Int -> Panel -> Html Msg
+selectedPanelView timeWithZone index panel =
+    Html.div [ class "card-board-panel" ]
+        [ Html.div [ class "card-board-columns" ]
+            (panel
+                |> Panel.columns timeWithZone index
+                |> List.map (\( n, cs ) -> column timeWithZone n cs)
+            )
+        ]
+
+
+column : TimeWithZone -> String -> List Card -> Html Msg
+column timeWithZone title cards =
+    Html.div [ class "card-board-column" ]
+        [ Html.div [ class "card-board-column-header" ]
+            [ Html.text title ]
+        , Html.Keyed.ul [ class "card-board-column-list" ]
+            (List.map (cardView timeWithZone) cards)
+        ]
+
+
+cardView : TimeWithZone -> Card -> ( String, Html Msg )
+cardView timeWithZone card =
+    let
+        cardId =
+            Card.id card
+
+        taskItem =
+            Card.taskItem card
+
+        taskItemId =
+            Card.taskItemId card
+
+        highlightAreaClass =
+            case Card.highlight timeWithZone card of
+                Card.HighlightCritical ->
+                    "critical"
+
+                Card.HighlightGood ->
+                    "good"
+
+                Card.HighlightImportant ->
+                    "important"
+
+                Card.HighlightNone ->
+                    ""
+    in
+    Html.li [ class "card-board-card cm-s-obsidian markdown-preview-view" ]
+        [ Html.div [ class ("card-board-card-highlight-area " ++ highlightAreaClass) ]
+            []
+        , Html.div [ class "card-board-card-content-area" ]
+            [ Html.input
+                [ type_ "checkbox"
+                , class "task-list-item-checkbox"
+                , onClick <| TaskItemToggled taskItemId
+                , checked <| TaskItem.isCompleted taskItem
+                ]
+                []
+            , Html.div [ class "card-board-card-title", id cardId ]
+                []
+            , cardTagsView (TaskItem.tags taskItem)
+                |> when (TaskItem.hasTags taskItem)
+            , subtasksView (Card.subtasks card)
+                |> when (TaskItem.hasSubtasks taskItem)
+            , notesView (Card.notesId card)
+                |> when (TaskItem.hasNotes taskItem)
+            , Html.div [ class "card-board-card-footer-area" ]
+                [ taskDueDate (TaskItem.due taskItem)
+                    |> when (TaskItem.isDated taskItem)
+                , cardActionButtons taskItemId (Card.editButtonId card)
+                ]
+            ]
+        ]
+        |> Tuple.pair cardId
+
+
+cardTagsView : List String -> Html Msg
+cardTagsView tags =
+    Html.div [ class "card-board-card-tag-area" ]
+        (List.map cardTagView tags)
+
+
+cardTagView : String -> Html Msg
+cardTagView tagText =
+    Html.div [ class ("card-board-card-tag tag-" ++ String.replace "/" "-" tagText) ]
+        [ Html.span [ class "cm-hashtag-begin cm-hashtag" ]
+            [ Html.text "#" ]
+        , Html.span [ class "cm-list-1 cm-hashtag cm-hashtag-end" ]
+            [ Html.text tagText ]
+        , Html.span [ class "cm-list-1" ]
+            [ Html.text " " ]
+        ]
+
+
+notesView : String -> Html Msg
+notesView notesId =
+    Html.div [ class "card-board-card-notes-area", id notesId ]
+        []
+
+
+subtasksView : List ( String, TaskItem ) -> Html Msg
+subtasksView subtasks =
+    Html.div [ class "card-board-card-subtasks-area" ]
+        [ Html.ul [ class "contains-task-list" ]
+            (List.map subtaskView subtasks)
+        ]
+
+
+subtaskView : ( String, TaskItem ) -> Html Msg
+subtaskView ( uniqueId, subtask ) =
+    Html.li [ class "card-board-card-subtask task-list-item" ]
+        [ Html.input
+            [ type_ "checkbox"
+            , class "task-list-item-checkbox"
+            , onClick <| TaskItemToggled <| TaskItem.id subtask
+            , checked <| TaskItem.isCompleted subtask
+            ]
+            []
+        , Html.div [ class "card-board-card-title", id uniqueId ]
+            []
+        ]
+
+
+taskDueDate : Maybe Date -> Html Msg
+taskDueDate dueDate =
+    Html.div [ class "card-board-card-action-area-due" ]
+        [ Html.text ("Due: " ++ dueDateString dueDate)
+        ]
+
+
+dueDateString : Maybe Date -> String
+dueDateString dueDate =
+    case dueDate of
+        Just date ->
+            Date.format "E, MMM ddd" date
+
+        Nothing ->
+            "n/a"
+
+
+cardActionButtons : String -> String -> Html Msg
+cardActionButtons taskItemId editButtonId =
+    Html.div [ class "card-board-card-action-area-buttons" ]
+        [ Html.div
+            [ class "card-board-card-action-area-button"
+            , onClickWithPreventDefault <| TaskItemEditClicked taskItemId
+            , id editButtonId
+            ]
+            [ FeatherIcons.edit
+                |> FeatherIcons.withSize 1
+                |> FeatherIcons.withSizeUnit "em"
+                |> FeatherIcons.toHtml []
+            ]
+        , Html.div
+            [ class "card-board-card-action-area-button"
+            , onClick <| TaskItemDeleteClicked taskItemId
+            ]
+            [ FeatherIcons.trash
+                |> FeatherIcons.withSize 1
+                |> FeatherIcons.withSizeUnit "em"
+                |> FeatherIcons.toHtml []
+            ]
+        ]
+
+
+
+-- HELPERS
+
+
+onClickWithPreventDefault : msg -> Html.Attribute msg
+onClickWithPreventDefault msg =
+    Html.Events.preventDefaultOn "click" (JD.map alwaysPreventDefault (JD.succeed msg))
+
+
+alwaysPreventDefault : msg -> ( msg, Bool )
+alwaysPreventDefault msg =
+    ( msg, True )
+
+
+empty : Html Msg
+empty =
+    Html.text ""
+
+
+when : Bool -> Html Msg -> Html Msg
+when shouldRender html =
+    if shouldRender then
+        html
+
+    else
+        empty
