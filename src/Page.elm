abc6af05e9692924010e3ee94f4ffeefceec0e58
rupert <rupert.voelcker@bt.com>
2022-09-30 09:38:50 +0100

    Cleanup: stop top of board from being obscured by page header
    use file-margin css variable!

--- src/Page.elm
+++ src/Page.elm
@@ -6,7 +6,7 @@ import Html.Attributes exposing (class)
 
 view : { content : Html msg, modal : Maybe (Html msg) } -> Html msg
 view { content, modal } =
-    Html.div [ class "card-board" ]
+    Html.div [ class "card-board-view" ]
         [ Html.div [ class "card-board-container" ]
             [ content
             , modal |> Maybe.withDefault (Html.text "")
