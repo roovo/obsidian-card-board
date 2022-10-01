a8ced0f9926e7566bcb0a5bfdc50541945d7ff9b
rupert <rupert.voelcker@bt.com>
2022-10-01 13:10:42 +0100

    Tags: parses anything starting with a #

--- src/Tag.elm
+++ src/Tag.elm
@@ -1,12 +1,31 @@
-module Tag exposing (parser)
+module Tag exposing
+    ( parser
+    , toString
+    )
 
-import Parser exposing (Parser)
+import Parser as P exposing ((|.), (|=), Parser)
 
 
 type Tag
     = Tag String
 
 
+toString : Tag -> String
+toString (Tag s) =
+    s
+
+
 parser : Parser Tag
 parser =
-    Parser.problem "oops"
+    P.succeed Tag
+        |. P.token "#"
+        |= P.andThen checkIfEmpty (P.getChompedString (P.chompWhile (always True)))
+
+
+checkIfEmpty : String -> Parser String
+checkIfEmpty parsedString =
+    if String.length parsedString == 0 then
+        P.problem "Empty string found"
+
+    else
+        P.succeed parsedString
--- tests/TagTests.elm
+++ tests/TagTests.elm
@@ -17,7 +17,13 @@ suite =
 parser : Test
 parser =
     describe "parser"
-        [ test "fails with just a '#'" <|
+        [ test "fails with an empty string" <|
+            \() ->
+                ""
+                    |> Parser.run Tag.parser
+                    |> Result.toMaybe
+                    |> Expect.equal Nothing
+        , test "fails with just a '#'" <|
             \() ->
                 "#"
                     |> Parser.run Tag.parser
@@ -29,18 +35,34 @@ parser =
                     |> Parser.run Tag.parser
                     |> Result.toMaybe
                     |> Expect.equal Nothing
+        , fuzz noHashAtStartFuzzer "parses strings that start with '#'" <|
+            \fuzzedTag ->
+                ("#" ++ fuzzedTag)
+                    |> Parser.run Tag.parser
+                    |> Result.map Tag.toString
+                    |> Expect.equal (Ok fuzzedTag)
         ]
 
 
 noHashAtStartFuzzer : Fuzzer String
 noHashAtStartFuzzer =
     let
-        removeHashAtStart : String -> String
-        removeHashAtStart a =
+        dropWhileHashAtStart : String -> String
+        dropWhileHashAtStart a =
             if String.startsWith "#" a then
-                String.dropLeft 1 a
+                dropWhileHashAtStart <| String.dropLeft 1 a
+
+            else
+                a
+
+        ensureNotEmpty : String -> String
+        ensureNotEmpty a =
+            if String.length a == 0 then
+                "a"
 
             else
                 a
     in
     Fuzz.string
+        |> Fuzz.map dropWhileHashAtStart
+        |> Fuzz.map ensureNotEmpty
