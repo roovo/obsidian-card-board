01c5c95e448ed4d0e64656b891c39a302039079d
rupert <rupert.voelcker@bt.com>
2022-10-01 10:47:45 +0100

    Tags: parser tests for some failure cases
    just a #
    and not starting with a #

--- tests/TagTests.elm
+++ tests/TagTests.elm
@@ -1,6 +1,7 @@
 module TagTests exposing (suite)
 
 import Expect
+import Fuzz exposing (Fuzzer)
 import Parser
 import Tag
 import Test exposing (..)
@@ -16,10 +17,30 @@ suite =
 parser : Test
 parser =
     describe "parser"
-        [ test "fails with an empty string" <|
+        [ test "fails with just a '#'" <|
             \() ->
-                ""
+                "#"
+                    |> Parser.run Tag.parser
+                    |> Result.toMaybe
+                    |> Expect.equal Nothing
+        , fuzz noHashAtStartFuzzer "fails with strings that don't start with '#'" <|
+            \fuzzedTag ->
+                fuzzedTag
                     |> Parser.run Tag.parser
                     |> Result.toMaybe
                     |> Expect.equal Nothing
         ]
+
+
+noHashAtStartFuzzer : Fuzzer String
+noHashAtStartFuzzer =
+    let
+        removeHashAtStart : String -> String
+        removeHashAtStart a =
+            if String.startsWith "#" a then
+                String.dropLeft 1 a
+
+            else
+                a
+    in
+    Fuzz.string
