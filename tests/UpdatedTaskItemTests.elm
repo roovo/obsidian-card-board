bf0ce3ff06029aaab48d97dbdf2ceeff89dee60d
roovo <rupert.voelcker@gmail.com>
2024-01-21 17:35:03 +0000

    PickDate: handle UpdatedTaskItem Dataview format completion

--- tests/UpdatedTaskItemTests.elm
+++ tests/UpdatedTaskItemTests.elm
@@ -88,12 +88,46 @@ toggleCompletion =
                             |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 @completed(1970-01-01T05:00:00+05:00) ^12345")
                 ]
             , describe "ObsidianTasks format"
-                [ test "outputs a completed task string (with a UTC CardBoard formatted completed tag)" <|
+                [ test "outputs a completed task string (with a Tasks formatted completed tag)" <|
                     \() ->
                         "- [ ] foo #tag1 bar #tag2 ^12345"
                             |> Parser.run TaskItemHelpers.basicParser
                             |> Result.map UpdatedTaskItem.init
                             |> Result.map (UpdatedTaskItem.toggleCompletion tasksCompletionSettings timeAtEpoc)
+                            |> Result.map UpdatedTaskItem.toString
+                            |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 ✅ 1970-01-01 ^12345")
+                ]
+            , describe "ObsidianDataview format"
+                [ test "outputs a completed task string (respects DataView NoCompletion)" <|
+                    \() ->
+                        "- [ ] foo #tag1 bar #tag2 ^12345"
+                            |> Parser.run TaskItemHelpers.basicParser
+                            |> Result.map UpdatedTaskItem.init
+                            |> Result.map (UpdatedTaskItem.toggleCompletion dataviewCompletionSettings timeAtEpoc)
+                            |> Result.map UpdatedTaskItem.toString
+                            |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 ^12345")
+                , test "outputs a completed task string (respects DataView custom completion string)" <|
+                    \() ->
+                        "- [ ] foo #tag1 bar #tag2 ^12345"
+                            |> Parser.run TaskItemHelpers.basicParser
+                            |> Result.map UpdatedTaskItem.init
+                            |> Result.map
+                                (UpdatedTaskItem.toggleCompletion
+                                    { dataviewCompletionSettings | dataviewTaskCompletion = DataviewTaskCompletion.Text "done" }
+                                    timeAtEpoc
+                                )
+                            |> Result.map UpdatedTaskItem.toString
+                            |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 [done:: 1970-01-01] ^12345")
+                , test "outputs a completed task string (respects DataView emoji completion string)" <|
+                    \() ->
+                        "- [ ] foo #tag1 bar #tag2 ^12345"
+                            |> Parser.run TaskItemHelpers.basicParser
+                            |> Result.map UpdatedTaskItem.init
+                            |> Result.map
+                                (UpdatedTaskItem.toggleCompletion
+                                    { dataviewCompletionSettings | dataviewTaskCompletion = DataviewTaskCompletion.Emoji }
+                                    timeAtEpoc
+                                )
                             |> Result.map UpdatedTaskItem.toString
                             |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 ✅ 1970-01-01 ^12345")
                 ]
@@ -103,6 +137,15 @@ toggleCompletion =
 
 
 -- HELPERS
+
+
+dataviewCompletionSettings : TaskCompletionSettings
+dataviewCompletionSettings =
+    { dataviewTaskCompletion = DataviewTaskCompletion.NoCompletion
+    , format = GlobalSettings.ObsidianDataview
+    , inLocalTime = False
+    , showUtcOffset = False
+    }
 
 
 noCompletionSettings : TaskCompletionSettings
@@ -144,48 +187,6 @@ timeAtEpoc =
 --
 --
 --
--- , test "given an INCOMPLETE item it outputs a string for a completed task in ObsidianDataview custom format" <|
---     \() ->
---         "- [ ] foo #tag1 bar #tag2 ^12345"
---             |> Parser.run TaskItemHelpers.basicParser
---             |> Result.map
---                 (TaskItem.toggleCompletion
---                     (DataviewTaskCompletion.Text "done")
---                     { format = GlobalSettings.ObsidianDataview
---                     , inLocalTime = False
---                     , showUtcOffset = False
---                     }
---                     { time = Time.millisToPosix 0, zone = Time.customZone 0 [] }
---                 )
---             |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 [done:: 1970-01-01] ^12345")
--- , test "given an INCOMPLETE item it outputs a string for a completed task in ObsidianDataview emoji format" <|
---     \() ->
---         "- [ ] foo #tag1 bar #tag2 ^12345"
---             |> Parser.run TaskItemHelpers.basicParser
---             |> Result.map
---                 (TaskItem.toggleCompletion
---                     DataviewTaskCompletion.Emoji
---                     { format = GlobalSettings.ObsidianDataview
---                     , inLocalTime = False
---                     , showUtcOffset = False
---                     }
---                     { time = Time.millisToPosix 0, zone = Time.customZone 0 [] }
---                 )
---             |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 ✅ 1970-01-01 ^12345")
--- , test "given an INCOMPLETE item it outputs a string for a completed task in ObsidianDataview no completion" <|
---     \() ->
---         "- [ ] foo #tag1 bar #tag2 ^12345"
---             |> Parser.run TaskItemHelpers.basicParser
---             |> Result.map
---                 (TaskItem.toggleCompletion
---                     DataviewTaskCompletion.NoCompletion
---                     { format = GlobalSettings.ObsidianDataview
---                     , inLocalTime = False
---                     , showUtcOffset = False
---                     }
---                     { time = Time.millisToPosix 0, zone = Time.customZone 0 [] }
---                 )
---             |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 ^12345")
 -- , test "given an item with an 'x' in the checkbox outputs a string for an incomplete task removing all formats of completed marks" <|
 --     \() ->
 --         "- [x] foo #tag1 bar #tag2 @completed(2020-03-22T00:00:00Z) @completed(2020-03-22T00:00:00-01:00) @completed(2020-03-22T00:00:00+01:00) @completed(2020-03-22T00:00:00) ✅ 1970-01-01 [done:: 2021-01-01] ^12345"
