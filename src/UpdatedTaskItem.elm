ff9c59230e62d9e199a8e04791915c272dd96752
roovo <rupert.voelcker@gmail.com>
2024-01-21 16:13:04 +0000

    PickDate: UpdatedTaskItem handles basic toggleCompletion

--- src/UpdatedTaskItem.elm
+++ src/UpdatedTaskItem.elm
@@ -4,7 +4,9 @@ module UpdatedTaskItem exposing
     , toggleCompletion
     )
 
-import TaskItem exposing (TaskItem)
+import Regex exposing (Regex)
+import String.Extra as SE
+import TaskItem exposing (Completion, TaskItem)
 
 
 
@@ -12,7 +14,12 @@ import TaskItem exposing (TaskItem)
 
 
 type UpdatedTaskItem
-    = UpdatedTaskItem TaskItem
+    = UpdatedTaskItem Changes TaskItem
+
+
+type alias Changes =
+    { changeCompletion : Bool
+    }
 
 
 
@@ -21,7 +28,7 @@ type UpdatedTaskItem
 
 init : TaskItem -> UpdatedTaskItem
 init =
-    UpdatedTaskItem
+    UpdatedTaskItem { changeCompletion = False }
 
 
 
@@ -29,8 +36,33 @@ init =
 
 
 toString : UpdatedTaskItem -> String
-toString (UpdatedTaskItem taskItem) =
-    TaskItem.originalText taskItem
+toString (UpdatedTaskItem changes taskItem) =
+    if changes.changeCompletion then
+        let
+            replaceCheckbox : TaskItem -> String -> String
+            replaceCheckbox t_ taskString =
+                let
+                    checkboxIndex : Int
+                    checkboxIndex =
+                        Regex.find checkboxRegex taskString
+                            |> List.head
+                            |> Maybe.map .index
+                            |> Maybe.withDefault 0
+                            |> (+) 2
+                in
+                SE.replaceSlice
+                    (toggledCheckbox t_)
+                    checkboxIndex
+                    (checkboxIndex + 3)
+                    taskString
+        in
+        taskItem
+            |> TaskItem.originalText
+            |> replaceCheckbox taskItem
+
+    else
+        taskItem
+            |> TaskItem.originalText
 
 
 
@@ -38,5 +70,137 @@ toString (UpdatedTaskItem taskItem) =
 
 
 toggleCompletion : UpdatedTaskItem -> UpdatedTaskItem
-toggleCompletion updatedTaskItem =
-    updatedTaskItem
+toggleCompletion (UpdatedTaskItem changes taskItem) =
+    UpdatedTaskItem { changes | changeCompletion = not changes.changeCompletion } taskItem
+
+
+
+-- PRIVATE
+
+
+checkboxRegex : Regex
+checkboxRegex =
+    Maybe.withDefault Regex.never <|
+        Regex.fromString "(-|\\*|\\+) \\[[ xX]\\]"
+
+
+toggledCheckbox : TaskItem -> String
+toggledCheckbox t_ =
+    case TaskItem.completion t_ of
+        TaskItem.Incomplete ->
+            "[x]"
+
+        _ ->
+            "[ ]"
+
+
+
+--
+--
+--
+--
+--
+-- toToggledString : DataviewTaskCompletion -> TaskCompletionSettings -> TimeWithZone -> TaskItem -> String
+-- toToggledString dataviewTaskCompletion taskCompletionSettings timeWithZone ((TaskItem fields_ _) as taskItem) =
+--     let
+--         blockLinkRegex : Regex
+--         blockLinkRegex =
+--             Maybe.withDefault Regex.never <|
+--                 Regex.fromString "(\\s\\^[a-zA-Z\\d-]+)$"
+--
+--         checkboxRegex : Regex
+--         checkboxRegex =
+--             Maybe.withDefault Regex.never <|
+--                 Regex.fromString "(-|\\*|\\+) \\[[ xX]\\]"
+--
+--         completionTag : TaskItem -> String
+--         completionTag t_ =
+--             case completion t_ of
+--                 Incomplete ->
+--                     TimeWithZone.completionString
+--                         dataviewTaskCompletion
+--                         taskCompletionSettings
+--                         timeWithZone
+--                         |> (\str ->
+--                                 if String.length str == 0 then
+--                                     ""
+--
+--                                 else
+--                                     " " ++ str
+--                            )
+--
+--                 _ ->
+--                     ""
+--
+--         insertCompletionTag : TaskItem -> String -> String
+--         insertCompletionTag t_ taskString =
+--             let
+--                 tagInserter : String -> String
+--                 tagInserter =
+--                     Regex.find blockLinkRegex taskString
+--                         |> List.head
+--                         |> Maybe.map .index
+--                         |> Maybe.withDefault (String.length taskString)
+--                         |> SE.insertAt (completionTag t_)
+--             in
+--             tagInserter taskString
+--
+--         regexReplacer : String -> (Regex.Match -> String) -> String -> String
+--         regexReplacer regex replacer original =
+--             case Regex.fromString regex of
+--                 Just r ->
+--                     Regex.replace r replacer original
+--
+--                 Nothing ->
+--                     original
+--
+--         removeCompletionTags : String -> String
+--         removeCompletionTags =
+--             let
+--                 dataviewRemover : String -> String
+--                 dataviewRemover =
+--                     case dataviewTaskCompletion of
+--                         DataviewTaskCompletion.NoCompletion ->
+--                             identity
+--
+--                         DataviewTaskCompletion.Emoji ->
+--                             identity
+--
+--                         DataviewTaskCompletion.Text t ->
+--                             regexReplacer (" \\[" ++ t ++ ":: \\d{4}-\\d{2}-\\d{2}\\]") (\_ -> "")
+--             in
+--             regexReplacer " @completed\\(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(?:(?:[+-]\\d{2}:\\d{2})|Z){0,1}\\)" (\_ -> "")
+--                 >> regexReplacer " ✅ \\d{4}-\\d{2}-\\d{2}" (\_ -> "")
+--                 >> dataviewRemover
+--
+--         replaceCheckbox : TaskItem -> String -> String
+--         replaceCheckbox t_ taskString =
+--             let
+--                 checkboxIndex : Int
+--                 checkboxIndex =
+--                     Regex.find checkboxRegex taskString
+--                         |> List.head
+--                         |> Maybe.map .index
+--                         |> Maybe.withDefault 0
+--                         |> (+) 2
+--             in
+--             SE.replaceSlice
+--                 (toggledCheckbox t_)
+--                 checkboxIndex
+--                 (checkboxIndex + 3)
+--                 taskString
+--
+--         toggledCheckbox : TaskItem -> String
+--         toggledCheckbox t_ =
+--             case completion t_ of
+--                 Incomplete ->
+--                     "[x]"
+--
+--                 _ ->
+--                     "[ ]"
+--     in
+--     fields_.originalText
+--         |> replaceCheckbox taskItem
+--         |> removeCompletionTags
+--         |> insertCompletionTag taskItem
+--
--- tests/UpdatedTaskItemTests.elm
+++ tests/UpdatedTaskItemTests.elm
@@ -11,6 +11,7 @@ suite : Test
 suite =
     concat
         [ toString
+        , toggleCompletion
         ]
 
 
@@ -24,4 +25,29 @@ toString =
                     |> Result.map UpdatedTaskItem.init
                     |> Result.map UpdatedTaskItem.toString
                     |> Expect.equal (Ok "- [x] foo")
+        ]
+
+
+toggleCompletion : Test
+toggleCompletion =
+    describe "toggleCompletion"
+        [ describe "NoCompletion format"
+            [ test "given an INCOMPLETE item it outputs a string for a completed task with no completed tag" <|
+                \() ->
+                    "- [ ] foo #tag1 bar #tag2 ^12345"
+                        |> Parser.run TaskItemHelpers.basicParser
+                        |> Result.map UpdatedTaskItem.init
+                        |> Result.map UpdatedTaskItem.toggleCompletion
+                        |> Result.map UpdatedTaskItem.toString
+                        |> Expect.equal (Ok "- [x] foo #tag1 bar #tag2 ^12345")
+            , test "returns the original text if completion is toggled twice" <|
+                \() ->
+                    "- [ ] foo #tag1 bar #tag2 ^12345"
+                        |> Parser.run TaskItemHelpers.basicParser
+                        |> Result.map UpdatedTaskItem.init
+                        |> Result.map UpdatedTaskItem.toggleCompletion
+                        |> Result.map UpdatedTaskItem.toggleCompletion
+                        |> Result.map UpdatedTaskItem.toString
+                        |> Expect.equal (Ok "- [ ] foo #tag1 bar #tag2 ^12345")
+            ]
         ]
